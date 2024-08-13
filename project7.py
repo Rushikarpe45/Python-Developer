c2dcac61eba8c7d2012e5959c905c1bbe4c98be3
Rushikarpe7771 <142646819+Rushikarpe45@users.noreply.github.com>
2024-08-13 19:44:26 +0530

    Create project7.py
    #WAP to input 2 int number, a and b
    #print True if a is greater than or equal to b. if not print False.

---
+++ project7.py
@@ -0,0 +1,12 @@
+#WAP to input 2 int number, a and b
+#print True if a is greater than or equal to b. if not print False.
+
+a=int(input("Enter your first number"))
+b=int(input("Enter your second number"))
+
+print(a>=b)
+
+# if(a>=b or a==b):
+#     print("True")
+# else:
+#     print("False")
