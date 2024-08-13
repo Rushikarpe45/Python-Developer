ab8a02e16c9c56871bf249f315a2cdadcb0b7e55
Rushikarpe7771 <142646819+Rushikarpe45@users.noreply.github.com>
2024-08-13 19:45:55 +0530

    Create project8.py
    # write to ask the user to enter name of their 3 favoriate movies & store them in a list

---
+++ project8.py
@@ -0,0 +1,20 @@
+# write to ask the user to enter name of their 3 favoriate movies & store them in a list
+
+movies=[]
+# movies1=input("enter a movie 1: ")
+# movies.append(movies1)
+# movies2=input("enter a movie 2: ")
+# movies.append(movies2)
+# movies3=input("enter a movie 3: ")
+# movies.append(movies3)
+
+# movies.append(movies1)
+# movies.append(movies2)
+# movies.append(movies3)
+
+
+movies.append(input("enter first movie:"))
+movies.append(input("enter second movie:"))
+movies.append(input("enter thierd movie:"))
+
+print(movies)
