4e21de8294a8ba1556ac2504d5edb0867e14db90
Rushikesh Karpe <142646819+Rushikarpe45@users.noreply.github.com>
2025-06-24 15:55:20 +0530

    Create Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,60 @@
+# vote
+# age=int(input("Enter the age: "))
+# if age>=18:
+#     print("Eligible for voting.")
+
+# even
+# no=int(input("enter the no.: "))
+# if no%2==0:
+#     print("the given no. is even.")
+
+# odd
+# no=int(input("enter the no.:"))
+# if no%2!=0:
+#     print(" the given no. is odd")
+
+# palindrome
+# st=input("enter the str: ")
+# if st[-1::]:
+#     print('the given str is palindrome.')
+    
+# check vowel
+# st=input("enter the str: ")
+# if st[0] in "a,e,i,o,u,A,E,I,O,U":
+#     print("hear is vowel")
+    
+# square 
+# no=int(input('enter the no.: '))
+# if no%2==0:
+#     print("square is ",no**2)
+
+# cube
+# no=int(input('enter the no.: '))
+# if (no%9==0) and (no%6==0):
+#     print("cube is ",no**3)
+
+# ascii
+# a=input("enter character: ")
+# if a.upper:
+#     print("the ascii value is: ",ord(a))
+
+# lower
+# a=input("enter character: ")
+# if a.lower:
+#     print("the is in lowercase.")
+
+# a=input("enter character: ")
+# if a.:
+#     print("the is in special chr.")
+
+# a=int(input("enter the no.: "))
+# if a>=100 and a<=999:
+#     print(" this no. is 3 digit.")
+
+# a=(input("enter the no.: "))
+# if int(a[-1])==5:
+#     print('the last digit is 5')
+
+# a=eval(input("enter the no.: "))
+# if type(a)== int or float or bool or complex:
+#     print("this  is single value datatype ")
